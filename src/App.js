bf19e5ed7c25c3e5cb7ccfba71fc1d1a962517bf
joyalcyriac <its.me.cj7p@gmail.com>
2024-01-08 15:05:33 +0530

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 
 import './App.css';
-
 import React from 'react';
 import { BrowserRouter as Router, Route, BrowserRouter, Routes} from 'react-router-dom';
 import UserTypeSelection from './componets/UserTypeSelection';
 import ClientRegistration from './componets/ClientRegistration';
 import WorkerRegistration from './componets/WorkerRegistration';
+import Home from './componets/Home';
+import Navigation from './componets/Navigation';
+import LoginPage from './componets/LoginPage';
 
 function App() {
   return (
     <div >
-          <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<UserTypeSelection method='get'/>} />
+     {/* <Navigation/> */}
+      <BrowserRouter>
+        <Navigation/>
+        <Routes>
+          <Route path="/" element={<Home method='get'/>} />
+          <Route path="/type" element={<UserTypeSelection method='post'/>} />
+          <Route path="/login" element={<LoginPage method='get'/>} />
           <Route path="/client" element={<ClientRegistration method='get' />} />
           <Route path="/worker" element={<WorkerRegistration method='get'/>} />
         </Routes>
